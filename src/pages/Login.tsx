06ed77bcbbc371033dca0a78ff7097d67d4a9d4a
Chenter <chenterphai61@gmail.com>
2025-06-15 17:21:16 +0700

    Modified

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,9 @@
-// import { LoginForm } from "@/components/material/LoginForm"
+import { LoginForm } from "@/components/material/LoginForm"
 import { Navigate } from "react-router";
 import { useAuth } from "@/middleware/AuthMiddleware";
-import { Auth } from '@supabase/auth-ui-react'
-import { ThemeSupa } from '@supabase/auth-ui-shared'
-import supabase from "@/utils/supabase";
+// import { Auth } from '@supabase/auth-ui-react'
+// import { ThemeSupa } from '@supabase/auth-ui-shared'
+// import supabase from "@/utils/supabase";
 
 const Login = () => {
 
@@ -11,13 +11,13 @@ const Login = () => {
 
     if (session) return <Navigate to={'/dashboard'} />
 
-    // return <div className="h-screen flex items-center justify-center">
-    //     <LoginForm />
-    // </div>
-
-    return <div className="h-screen mx-auto w-96 mt-32">
-        <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
+    return <div className="h-screen flex items-center justify-center">
+        <LoginForm />
     </div>
+
+    // return <div className="h-screen mx-auto w-96 mt-32">
+    //     <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
+    // </div>
 }
 
 export default Login
